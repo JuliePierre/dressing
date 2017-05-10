54f227f91e082f5730c86f07a531a4f99f7bc4a0
Julie Pierre <julie.pierre3@gmail.com>
2017-05-10 14:42:33 +0200

    outil de capture photo pour creation nouvelle tenue ok

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -9,5 +9,20 @@ class OutfitsController < ApplicationController
   end
 
   def create
+    @outfit = Outfit.new(outfit_params)
+    @outfit.user = current_user
+
+    if @outfit.save
+      redirect_to outfit_path(@outfit)
+    else
+      render :new
+    end
   end
+
+  private
+
+  def outfit_params
+    params.require(:outfit).permit(:name, :photo)
+  end
+
 end
--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -1,4 +1,6 @@
 class Outfit < ApplicationRecord
   belongs_to :user
   has_attachment :photo
+
+  validates :name, presence: true
 end
