57b07827d9c687f86eb09d53134690778354e388
Julie Pierre <julie.pierre3@gmail.com>
2017-05-11 20:14:04 +0200

    page show user en cours

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @user_outfits = Outfit.where(" outfits.user_id = ? ", @user.id)
+    @user_friends = @user.friendships.where("is_accepted = ?", true)
+    @friend_requests = @user.inverse_friends.joins(:friendships).where.not("friendships.is_accepted = ?", true)
+    @made_friend_requests = @user.friendships.select do |friendship|
+      !friendship.is_accepted
+    end
   end
 end
