efc8ff96e64ff74a1e6aab3e3ffbef118119b792
Julie Pierre <julie.pierre3@gmail.com>
2017-05-11 12:40:45 +0200

    réseaus social : viré la possibilité d'ajouter la même personne 2 fois

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,13 +1,18 @@
 class FriendshipsController < ApplicationController
 
   def create
-    @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
-    if @friendship.save
-      flash[:notice] = "Added friend."
-      redirect_to root_url
+    if current_user.friendships.find_by_friend_id(params[:friend_id])
+      flash[:notice] = "Pas possible. Vous êtes déjà amis"
+      redirect_to user_path(current_user)
     else
-      flash[:notice] = "Unable to add friend."
-      redirect_to root_url
+      @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+      if @friendship.save
+        flash[:notice] = "Added friend."
+        redirect_to user_path(current_user)
+      else
+        flash[:notice] = "Unable to add friend."
+        redirect_to user_path(current_user)
+      end
     end
   end
 
