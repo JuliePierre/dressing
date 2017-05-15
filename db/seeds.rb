a8bce527ef7e07718f6a667745f98df4ff339058
Julie Pierre <julie.pierre3@gmail.com>
2017-05-15 19:17:28 +0200

    changement seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+MissingItem.destroy_all
 a = MissingItem.create(name: "Chapeau")
 b = MissingItem.create(name: "Foulard")
 c = MissingItem.create(name: "Pochette")
