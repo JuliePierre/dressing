54a4ae5d1617edfa60759b3373437c2331465e33
Julie Pierre <julie.pierre3@gmail.com>
2017-06-29 11:22:01 +0200

    wip

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :landing, :home ]
+  layout "landing", only: [ :landing ]
+
+  def landing
+  end
 
   def home
+    @dressing_items = DressingItem.order(:id).first(3)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', sessions: 'users/sessions', registrations: 'users/registrations' }
 
-  root to: 'pages#home'
+  root to: 'pages#landing'
+  get 'accueil', to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :outfits, only: [ :index, :new, :create, :show, :update ]
   resources :users, only: [:index, :show] do
