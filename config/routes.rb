cdd44a435df867508b9cfa68aa84991275112bbf
Julie Pierre <julie.pierre3@gmail.com>
2017-06-29 10:05:32 +0200

    exit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount Attachinary::Engine => "/attachinary"
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', sessions: 'users/sessions', registrations: 'users/registrations' }
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :outfits, only: [ :index, :new, :create, :show, :update ]
