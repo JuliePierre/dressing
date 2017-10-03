e71dbf8c6b06d6dec1244482ebe193f15a889211
Julie Pierre <julie.pierre3@gmail.com>
2017-10-03 11:07:19 +0200

    wip

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -2,4 +2,12 @@ class ShoppingCart < ApplicationRecord
   acts_as_shopping_cart
 
   belongs_to :user
+
+  def cart_price
+    sum = 0
+    self.shopping_cart_items.each do |item|
+      sum += item.price
+    end
+    return sum
+  end
 end
