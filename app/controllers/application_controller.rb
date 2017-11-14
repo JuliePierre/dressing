fb3c1ea12dd086f764c78085bac83e06a772b5dc
Julie Pierre <julie.pierre3@gmail.com>
2017-11-14 15:12:33 +0100

    changement total wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
+    dashboard_user_path(current_user)
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
     root_path
   end
 end
