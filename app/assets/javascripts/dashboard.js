c841609f5f1301905c56b27673a315e9ac59a807
Julie Pierre <julie.pierre3@gmail.com>
2017-11-15 13:05:02 +0100

    wip

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -8,6 +8,9 @@ $(document).ready(function(){
   $( "#link-to-mondressing" ).on( "click", function() {
     $( "#mondressing-link a" ).trigger( "click" );
   });
+  $( "#link-to-ajouter" ).on( "click", function() {
+    $( "#ajouter-link a" ).trigger( "click" );
+  });
   // $( "#monreseau-link" ).on( "click", function() {
   //   $('#mesamis-link a').trigger('click')
   // });
