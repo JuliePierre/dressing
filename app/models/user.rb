236a6db162a13bda95e0537c8d5669be6c54e5b4
Julie Pierre <julie.pierre3@gmail.com>
2017-11-17 14:30:30 +0100

    wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :gender, presence: true
 
   def self.find_for_facebook_oauth(auth)
     user_params = auth.slice(:provider, :uid)
