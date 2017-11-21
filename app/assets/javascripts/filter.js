ff4754d63696d5b8f15c8e2d8d3f396fd94e3c46
Julie Pierre <julie.pierre3@gmail.com>
2017-11-21 18:13:46 +0100

    filter wip

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -1,3 +1,9 @@
-$('#filtering form label').change(function(){
+$('#filtering form label').click(function(event){
+  var id = "#" + $(this).attr("for");
+  if ($(id).prop('checked') == true) {
+    $(id).prop('checked', false);
+  } else {
+    $(id).prop('checked', "checked");
+  }
   $('#filter-btn').trigger('click');
 });
